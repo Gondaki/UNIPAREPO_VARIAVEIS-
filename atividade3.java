7d7aff6afdcb2641a883fee5351ba79e34ebfcc9
Gondaki <160682803+Gondaki@users.noreply.github.com>
2024-02-20 22:13:17 -0300

    Create atividade3.java

---
+++ atividade3.java
@@ -0,0 +1,17 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        
+    String NomeDoAluno = "joao felipe";
+    int IdadeDoAluno = 22;
+    int NotaDaProva1 = 8;
+    int NotaDaProva2 = 8;
+    int MediaFinal =8;
+        
+        System.out.println(MediaFinal);
+    
+        
+   
+    }
+    
+    
+}
